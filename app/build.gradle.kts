7c8bbbfa2d991e42e2ed0b540c0bc0124b967055
MDAlviano <mdalviano.21122@gmail.com>
2025-02-14 00:39:45 +0700

    make WalletSection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.alvin.belajar.banking.app"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.alvin.belajar.banking.app"
--- app/src/main/java/com/alvin/belajar/banking/app/MainActivity.kt
+++ app/src/main/java/com/alvin/belajar/banking/app/MainActivity.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.alvin.belajar.banking.app.components.BottomNavigationBar
+import com.alvin.belajar.banking.app.components.WalletSection
 import com.alvin.belajar.banking.app.ui.theme.BankingAppUITheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +30,51 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BankingAppUITheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // A scaffold container using the 'background' color from the theme
+
+                SetBarColor(color = MaterialTheme.colorScheme.background)
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    containerColor = MaterialTheme.colorScheme.background
+                ) {
+                    HomeScreen()
                 }
             }
         }
     }
+
+    @Composable
+    private fun SetBarColor(color: Color) {
+        val systemUiController = rememberSystemUiController()
+        SideEffect {
+            systemUiController.setSystemBarsColor(
+                color = color
+            )
+        }
+    }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun HomeScreen() {
+    Scaffold(
+        bottomBar = {
+            BottomNavigationBar()
+        }
+    ) { padding ->
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BankingAppUITheme {
-        Greeting("Android")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) {
+            WalletSection()
+//        CardsSection()
+            Spacer(modifier = Modifier.height(16.dp))
+//        FinanceSection()
+//        CurrenciesSection()
+        }
+
     }
 }
